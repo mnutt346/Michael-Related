8bae508576c4e2b231f3ccd94b2e532a4f080664
gizmo <gizmo@Gremlin.local>
2019-01-03 15:11:29 -0600

    renders 2 components

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+//import Pledge from '../../../Mike-JumpStart-Pledge/client/components/pledge.jsx';
 
 class App extends Component {
     constructor(props) {
@@ -8,6 +9,7 @@ class App extends Component {
     render() {
         return (
             <div>
+                <Pledge />
                 <Related />
             </div>
         )
